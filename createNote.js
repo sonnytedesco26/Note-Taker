25b08847eae523f1af46756109805b02e375cc32
Sonny Tedesco <sonny.tedesco@darkmatter2bd.com>
2022-03-20 17:51:38 -0400

    wrote up delete function

--- createNote.js
+++ createNote.js
@@ -14,11 +14,11 @@ expr.get('/api/notes', (req, res) =>{
 });
 
 expr.get('/notes', (req, res) => {
-    res.sendFile(path.join(_dirname, './public/index.html'));
+    res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
 expr.get('/', (req, res) => {
-    res.sendFile(path.join(_dirname, './public/index.html'));
+    res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
 expr.post('/api/notes', (req, res) =>{
@@ -39,13 +39,27 @@ function createNote(main, noteArray){
 
     noteArray.push(newNote);
 
-    fs.writeFileSync(path.join(_dirname, './db/db.json'),
-    JSON.stringify(noteArray, null, 2)
+    fs.writeFileSync(path.join(__dirname, './db/db.json'),
+    JSON.stringify(noteArray, null, 1)
     );
 
     return newNote;
 }
 
 function deleteNote(id, noteArray){
-    
+    for(i=0;i<noteArray.length;i++){
+        var note = noteArray[i];
+
+        if(note.id == id){
+            noteArray.splice(i, 1);
+            fs.writeFileSync(path.join(__dirname, './db/db.json'), JSON.stringify(noteArray, null, 1)
+            );
+            break;
+        }
+    }
 }
+
+expr.delete('/api/notes/:id', (req, res) =>{
+    deleteNote(req.params.id, notes);
+    res.json(true);
+});
