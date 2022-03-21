9390a3a87544d5d85ef32580da17d7e6c6ff9ebf
Sonny Tedesco <sonny.tedesco@darkmatter2bd.com>
2022-03-20 20:21:31 -0400

    get not working, fixed stuff for the page publish

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const fs = require('fs');
-const express = require("express");
+const express = require('express');
 const path = require('path');
 const PORT = process.env.PORT || 3000;
 const expr = express();
