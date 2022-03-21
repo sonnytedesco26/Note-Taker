f1f16ca8ee12150ab9e4331eaf0411d43e693d3c
Sonny Tedesco <sonny.tedesco@darkmatter2bd.com>
2022-03-20 23:10:34 -0400

    wrote up readme, added comments, recorded video. Also emptied db.json after testing

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-# Note Taker Starter Code
+# Note Taker
+LINK:
+https://sonnytedesco26.github.io/Note-Taker/
+
+For this project, the goal was to create a server.js backend to work with the supplied front end to make a note taker app.
+
+## Installation
+
+In order to use the application, first clone the repo. Then, using a terminal in the root folder of the project, run npm init if not done already. Once that is done, run npm installs for express and fs. 
+
+## Using the application
+
+![readmegif](/public/assets/images/readmegif.gif)
+
+Once these packages are installed, all that's left to do is to run server.js using the by entering into the terminal (in the project root directory): node server.js
+
+This will kick off the express.js server (localhost3001)
+
+Now in your web browser url, enter: http://localhost:3001/
+
+There is a save button that only shows when the note is ready (has both a title and text in the body).
+The plus button lets you start a new note without saving whatever you have currently entered.
+
+
--- server.js
+++ server.js
@@ -1,17 +1,21 @@
+//need our fs and express packages for program to work
 const fs = require('fs');
 const express = require('express');
 const path = require('path');
+//declaring port number
 const PORT = process.env.PORT || 3001;
 const expr = express();
 const cors = require('cors');
 
 const notes = require('./db/db.json');
-
+//needed first variable name to be BODY and not MAIN to fit in with the index.js
 function createNote(body, noteArray){
     const newNote = body;
+    //if array doesn't exist, make empty one
     if(!Array.isArray(noteArray)){
         noteArray = [];
     }
+    //if array empty, push your first note in
     if(noteArray.length == 0){
         noteArray.push(0);
     }
@@ -19,27 +23,29 @@ function createNote(body, noteArray){
     noteArray[0]++;
 
     noteArray.push(newNote);
-
+    //2nd parameter of stringify null, cause we don't want a replacer
     fs.writeFileSync(path.join(__dirname, './db/db.json'), JSON.stringify(noteArray, null, 2)
     );
-
+    //throw that note back out
     return newNote;
 }
 
-
+//report that server is running in the terminal
 expr.listen(PORT, () => {
     console.log(`port: ${PORT}`);
 });
-
+//need to make express public to get port working
 expr.use(express.static('public'));
 expr.use(express.urlencoded({extended: true}));
 expr.use(express.json());
+//cors was giving issues without
 expr.use(cors());
-
+//slice(1) cause you only want that part of the returned json
 expr.get('/api/notes', (req, res) =>{
+    //renders note history on left (whenever the page reloads, or notes are added/deleted)
     res.json(notes.slice(1));
 });
-
+//get everything for the response
 expr.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, './public/notes.html'));
 });
@@ -51,18 +57,19 @@ expr.get('/', (req, res) => {
 expr.get('*', (req, res) =>{
     res.sendFile(path.join(__dirname, './public/index.html'));
 })
-
+//for the post, createNote is called
 expr.post('/api/notes', (req, res) =>{
     var newNote = createNote(req.body, notes);
     res.json(newNote);
 })
 
-
+//for every note in the array, check if the id matches what you wanna get rid of
 function deleteNote(id, noteArray){
     for(i=0;i<noteArray.length;i++){
         let note = noteArray[i];
 
         if(note.id == id){
+            //splice is so that we only delete based off the ID returned from the json
             noteArray.splice(i, 1);
             fs.writeFileSync(path.join(__dirname, './db/db.json'), JSON.stringify(noteArray, null, 2)
             );
@@ -70,7 +77,7 @@ function deleteNote(id, noteArray){
         }
     }
 }
-
+//delete whatever you choose to based on each note's id in the array
 expr.delete('/api/notes/:id', (req, res) =>{
     deleteNote(req.params.id, notes);
     res.json(true);
