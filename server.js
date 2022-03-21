3e9bcbfdf9ba41deb28b9f3be16a3777e0bdba14
Sonny Tedesco <sonny.tedesco@darkmatter2bd.com>
2022-03-20 21:45:18 -0400

    seeing if tests on published page work

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const express = require('express');
 const path = require('path');
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 const expr = express();
 const cors = require('cors');
 
